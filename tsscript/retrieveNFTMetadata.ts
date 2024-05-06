6b1be196b0c91c70e177fdea436113a3c8b31d60
Luis Videla <luisvid@gmail.com>
2024-05-05 21:30:02 -0300

    feat: fetch nft metadata script

---
+++ tsscript/retrieveNFTMetadata.ts
@@ -0,0 +1,67 @@
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+import abi_MockNFT from "../abi/contracts/MockNFT.sol/MockNFT.json";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Interface for the expected structure of NFT metadata
+interface NFTAttribute {
+  trait_type: string;
+  value: string;
+}
+
+interface NFTMetadata {
+  name: string;
+  description: string;
+  image: string;
+  strength: number;
+  attributes: NFTAttribute[];
+}
+
+/**
+ * Function to fetch NFT metadata from a blockchain
+ */
+async function getNFTMetadata() {
+  const { PRIVATE_KEY, API_KEY_INFURA } = process.env;
+
+  if (!PRIVATE_KEY || !API_KEY_INFURA) {
+    console.error("Please ensure your .env file includes PRIVATE_KEY and API_KEY_INFURA.");
+    return;
+  }
+
+  const SEPOLIA_URL = `https://sepolia.infura.io/v3/${API_KEY_INFURA}`;
+  const provider = new ethers.JsonRpcProvider(SEPOLIA_URL);
+  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+
+  const mockNFTAddress = "0x6351487b7bb81a14f6120A888AC5Ee5E1ec32Aa2";
+  const tokenId = 3;
+
+  const mockNFT = new ethers.Contract(mockNFTAddress, abi_MockNFT, wallet);
+  try {
+    const result = await mockNFT.tokenURI(tokenId);
+    const ipfsURL = addIPFSProxy(result);
+    console.log("IPFS URL\n", ipfsURL);
+
+    const response = await fetch(ipfsURL);
+    const metadata: NFTMetadata = await response.json();
+    console.log("NFT Metadata\n", metadata);
+
+    if (metadata.image) {
+      const image = addIPFSProxy(metadata.image);
+      console.log("NFT Image URL\n", image);
+    }
+  } catch (error) {
+    console.error("Failed to fetch NFT metadata:", error);
+  }
+}
+
+// Function to convert an IPFS hash to a URL using a public gateway
+function addIPFSProxy(ipfsHash: string): string {
+    const URL = "https://ipfs.io/ipfs/";
+    const hash = ipfsHash.replace(/^ipfs?:\/\//, "");
+    return URL + hash;
+  }
+  
+  getNFTMetadata();
